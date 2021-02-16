5fe28bfdf721dc7fb8e1d917ba22e0570f357bc0
Aksaule <aksaule124026@gmail.com>
2021-02-16 12:11:07 +0600

    Work with Urls and product, login files

--- yogurt/yogurtshop/urls.py
+++ yogurt/yogurtshop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('regin/', views.regin, name='regin'),
 ]
--- yogurt/yogurtshop/views.py
+++ yogurt/yogurtshop/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def products(request):
+    return render(request,'products.html')
+
+def regin(request):
+    return render(request,'regin.html')
