a0e4abdad43c6b9d8dd604860d79f6dd8afb0377
Aksaule <aksaule124026@gmail.com>
2021-02-18 22:00:07 +0600

    Working with admin panel, db, dynamic

--- yogurt/yogurtshop/views.py
+++ yogurt/yogurtshop/views.py
@@ -1,8 +1,10 @@
+from .models import Product
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
+    prod = Product.objects.all()
     return render(request,'home.html')
 
 def products(request):
